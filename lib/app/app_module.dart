957485ae088f45796cb253bf275058c31ccc8356
ANDREIA RATZLAFF SCHERER <ALU201220777@uniritter.local>
2019-10-18 11:38:02 -0300

    Inclusão do bloco pet e configuração do firebase

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,3 +1,5 @@
+import 'package:imypet/app/pages/pet/pet_repository.dart';
+import 'package:imypet/app/pages/pet/pet_bloc.dart';
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
 import 'package:imypet/app/app_widget.dart';
@@ -6,11 +8,14 @@ import 'package:imypet/app/app_bloc.dart';
 class AppModule extends ModuleWidget {
   @override
   List<Bloc> get blocs => [
+        Bloc((i) => PetBloc()),
         Bloc((i) => AppBloc()),
       ];
 
   @override
-  List<Dependency> get dependencies => [];
+  List<Dependency> get dependencies => [
+        Dependency((i) => PetRepository()),
+      ];
 
   @override
   Widget get view => AppWidget();
---
+++ lib/app/pages/pet/pet_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class PetPage extends StatefulWidget {
+  @override
+  _PetPageState createState() => _PetPageState();
+}
+
+class _PetPageState extends State<PetPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Pet"),
+      ),
+      body: Column(
+        children: <Widget>[],
+      ),
+    );
+  }
+}
