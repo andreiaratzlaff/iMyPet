d67e5721370765d8950ff48f2474e1bed2928aa0
ANDREIA RATZLAFF SCHERER <ALU201220777@uniritter.local>
2019-10-25 10:58:24 -0300

    Lógica (Bloc) do Per

--- lib/app/app_bloc.dart
+++ lib/app/app_bloc.dart
@@ -1,7 +1,19 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:imypet/app/app_module.dart';
+import 'package:imypet/app/pages/pet/pet_repository.dart';
 
 class AppBloc extends BlocBase {
   //dispose will be called automatically by closing its streams
+var _repository = AppModule.to.getDependency<PetRepository>();
+
+
+get pet => _repository.pet;
+
+
+void delete(String _documentId) => _repository.delete(_documentId);
+
+
+
   @override
   void dispose() {
     super.dispose();
