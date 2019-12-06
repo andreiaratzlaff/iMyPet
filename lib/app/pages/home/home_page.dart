5a4c2be12634863dd58eaf39adf81b1893d206e0
unknown <matheusmottadesouza@gmail.com>
2019-12-06 02:51:43 -0300

    Carousel

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:iMyPet/app/menu/drawer.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class HomePage extends StatefulWidget {
   final String title;
@@ -46,7 +47,30 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       body: Column(
-        children: <Widget>[],
+          children: <Widget>[
+            CarouselSlider(
+            height: 400.0,
+            items: [
+              'https://cdn.pixabay.com/photo/2015/07/31/11/43/bordeaux-869006_960_720.jpg',
+              'https://cdn.pixabay.com/photo/2019/09/14/23/14/dogs-4477058_960_720.jpg',
+              'https://cdn.pixabay.com/photo/2017/09/25/13/14/dog-2785077_960_720.jpg',
+              'https://cdn.pixabay.com/photo/2015/06/08/15/02/pug-801826_960_720.jpg'
+              ].map((i) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                    width: MediaQuery.of(context).size.width,
+                    margin: EdgeInsets.symmetric(horizontal: 5.0),
+                    decoration: BoxDecoration(
+                      color: Colors.amber
+                    ),
+                    child: Image.network(i, fit: BoxFit.fill)
+                  );
+                },
+              );
+            }).toList(),
+          )
+        ],
       ),
 
       
